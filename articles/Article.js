30a1d830074885496fba5c96334456323b8f46b0
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-20 17:20:22 -0300

    Criação da página de cadastro de categorias

--- articles/Article.js
+++ articles/Article.js
@@ -2,14 +2,14 @@ const Sequelize = require('sequelize')
 const connection = require('../database/database')
 const Category = require('../categories/Category')
 
-const Article = connection.define('articles',{
-    title:{
+const Article = connection.define('articles', {
+    title: {
         type: Sequelize.STRING,
         allowNull: false
-    },slug:{
+    }, slug: {
         type: Sequelize.STRING,
         allowNull: false
-    },body:{
+    }, body: {
         type: Sequelize.TEXT,
         allowNull: false
     }
@@ -17,5 +17,7 @@ const Article = connection.define('articles',{
 
 Article.belongsTo(Category)
 Category.hasMany(Article)
+
+//Article.sync({force:true})
 
 module.exports = Article
--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/categories", (req, res) => {
-    res.send("ROTA DE CATEGORIAS")
+router.get("/admin/categories/new", (req, res) => {
+    res.render("admin/categories/new")
 })
 
 module.exports = router
