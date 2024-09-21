1ebb0ef860e475ad8e07267ddb826ab503d129de
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-21 15:50:05 -0300

    Listando todos os artigos do banco na pagina index

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,14 @@ const Article = require("../articles/Article")
 const slugify = require("slugify")
 
 router.get("/admin/articles", (req, res) => {
-    res.render("admin/articles/index")
+
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render("admin/articles/index", { articles: articles })
+    })
+
+
 })
 
 router.get("/admin/articles/new", (req, res) => {
