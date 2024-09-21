010f6082f05cd1abf0bf2c8124f2c696219d0acc
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-21 15:14:44 -0300

    Implementação do botão de selecionar categoria

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("../categories/Category")
 
 router.get("/articles", (req, res) => {
     res.send("ROTA DE ARTIGOS")
 })
 
 router.get("/admin/articles/new", (req, res) => {
-    res.render("admin/articles/new")
+
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", { categories: categories })
+    })
 })
 
 module.exports = router
