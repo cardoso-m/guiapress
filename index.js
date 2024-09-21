880d0271c0e6565364b24839d99157b772c3b865
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-21 17:56:22 -0300

    Atualizando a página de exibição unica de uma categoria e configurando a exibiçao em uma formatação correta

--- index.js
+++ index.js
@@ -29,8 +29,30 @@ app.use("/", categoriesController)
 
 app.get("/", (req, res) => {
 
-    Article.findAll().then(articles =>{
-        res.render("index", {articles: articles})
+    Article.findAll({
+        order: [
+            ["id", "DESC"]
+        ]
+    }).then(articles => {
+        res.render("index", { articles: articles })
+    })
+})
+
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug
+
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if (article != undefined) {
+            res.render("article", { article: article })
+        } else {
+            res.redirect("/")
+        }
+    }).catch(error => {
+        res.redirect("/")
     })
 })
 
