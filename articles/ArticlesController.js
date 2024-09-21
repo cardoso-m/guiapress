2b9d4916cb1c5a7fc88a6a387f6895a64b49d09a
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-21 15:26:43 -0300

    Adicionando a rota de salvarr artigos e salvar no banco de dados

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,15 +1,31 @@
 const express = require("express")
 const router = express.Router()
 const Category = require("../categories/Category")
+const Article = require("../articles/Article")
+const slugify = require("slugify")
 
 router.get("/articles", (req, res) => {
     res.send("ROTA DE ARTIGOS")
 })
 
 router.get("/admin/articles/new", (req, res) => {
-
     Category.findAll().then(categories => {
         res.render("admin/articles/new", { categories: categories })
+    })
+})
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title
+    var category = req.body.category
+    var body = req.body.body
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        categoryId: category,
+        body: body
+    }).then(() => {
+        res.redirect("/articles")
     })
 })
 
