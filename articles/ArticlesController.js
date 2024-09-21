ab4e94cb1258fc21953e9e0db30c35ba64b53efb
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-21 17:18:41 -0300

    Criação da página de exibição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -36,4 +36,22 @@ router.post("/articles/save", (req, res) => {
     })
 })
 
+router.post("/admin/articles/delete", (req, res) => {
+    var id = req.body.id
+
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Article.destroy({
+                where: { id: id }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            })
+        } else {
+            res.redirect("/admin/articles")
+        }
+    } else {
+        res.redirect("/admin/articles")
+    }
+})
+
 module.exports = router
