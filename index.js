b7a7e642f41d0452dd0744e9e0bbece61c918ab2
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-23 12:28:55 -0300

    Filtrando artigos por categoria

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ app.get("/", (req, res) => {
             ["id", "DESC"]
         ]
     }).then(articles => {
-        res.render("index", { articles: articles })
+        Category.findAll().then(categories => {
+            res.render("index", { articles: articles, categories: categories })
+        })
     })
 })
 
@@ -47,13 +49,39 @@ app.get("/:slug", (req, res) => {
         }
     }).then(article => {
         if (article != undefined) {
-            res.render("article", { article: article })
+            Category.findAll().then(categories => {
+                res.render("article", { article: article, categories: categories })
+            })
         } else {
             res.redirect("/")
         }
     }).catch(error => {
         res.redirect("/")
     })
+})
+
+app.get("/categories/:slug", (req, res) => {
+    var slug = req.params.slug
+
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: Article }]
+    }).then(category => {
+
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", { articles: category.articles, categories: categories })
+            })
+        } else {
+            res.redirect("/")
+        }
+
+    }).catch(error => {
+        res.redirect("/")
+    })
+
 })
 
 app.listen(8000, () => {
