d628af816cb6f7b68a04979bd9aa8f725de80f08
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-23 13:30:05 -0300

    Adicionando o conteúdo do artigo dentro do TinyMCE

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -58,9 +58,9 @@ router.get("/admin/articles/edit/:id", (req, res) => {
     var id = req.params.id
 
     Article.findByPk(id).then(article => {
-        if (article = !undefined) {
+        if (article != undefined) {
             Category.findAll().then(categories => {
-                res.render("admin/articles/edit", { categories: categories })
+                res.render("admin/articles/edit", { article: article, categories: categories })
             })
         } else {
             res.redirect("/")
